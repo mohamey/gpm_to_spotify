7469bfdf79817917b491053987df53f81a7610a8
Yasir Mohamed <mohamey@tcd.ie>
2019-12-25 14:23:20 +0000

    Complete end to end flow of GPM To Spotify, including track upload

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+from exceptions.runtime_exceptions import InvalidConfigException
+from json import load
 from wrappers import gpm_wrapper, spotify_wrapper
 
 if __name__ == '__main__':
-    oauth_path = "Cred Path Here."
+    with open('.config.json') as config_file:
+        config = load(config_file)
 
-    gpm_wrapper = gpm_wrapper.Wrapper()
+    if not config:
+        raise InvalidConfigException("Config is empty")
+
+    gpm_wrapper = gpm_wrapper.Wrapper(config['gpm'])
 
     # Perform oauth (if necessary), then login to the service
-    gpm_wrapper.handle_auth_flow(oauth_path)
+    gpm_wrapper.handle_auth_flow()
     gpm_wrapper.get_song_library()
     gpm_wrapper.map_song_library_to_tracks()
-    track_list = gpm_wrapper.get_tracks()[:100]
+    track_list = gpm_wrapper.get_tracks()
 
-    spotify_wrapper = spotify_wrapper.SpotifyWrapper()
+    spotify_wrapper = spotify_wrapper.SpotifyWrapper(config['spotify'])
     spotify_wrapper.handle_auth()
-    for track in track_list:
-        spotify_wrapper.find_track(track)
+    spotify_wrapper.find_tracks(track_list)
+    spotify_wrapper.add_saved_tracks()
+
+    print(f"Matched {len(spotify_wrapper.mapped_tracks)} of {len(track_list)} from Google Play Music.")
 
     print("Finished!")
--- wrappers/spotify_wrapper.py
+++ wrappers/spotify_wrapper.py
@@ -1,4 +1,4 @@
-from exceptions.spotify_exceptions import InvalidAuthTokenReceived
+from exceptions.spotify_exceptions import InvalidAuthTokenReceived, InvalidTrackUsed
 from fuzzywuzzy import fuzz
 from json import load
 from meta.structures.track import Track
@@ -6,44 +6,77 @@ from spotipy import Spotify
 import re
 import spotipy.util as util
 
+
 class SpotifyWrapper:
-    def __init__(self):
+    def __init__(self, config):
         self.__spotify = Spotify()
         self.__scope = 'user-library-read,user-library-modify'
         self.__redirect_url = 'http://localhost:8080/redirect'
-        self.__load_secrets()
+        self.__client_id = config['client']['id']
+        self.__client_secret = config['client']['secret']
+        self.__user = config['user']
         self.mapped_tracks = []
         self.unmapped_tracks = []
 
-    def handle_auth(self):
-        token = util.prompt_for_user_token(self.__user, self.__scope, self.__client_id,\
-            self.__client_secret, self.__redirect_url)
+    # Handle authenticating this app with Spotify for a given user
+    def handle_auth(self, token=None):
+        if not token:
+            token = util.prompt_for_user_token(self.__user, self.__scope, self.__client_id,\
+                self.__client_secret, self.__redirect_url)
 
         if not token or len(token) == 0:
             raise InvalidAuthTokenReceived("Token is empty.")
 
         self.__spotify = Spotify(auth=token)
 
+    # Get list of songs saved by the user
     def get_user_saved_tracks(self):
-        results = self.__spotify.current_user_saved_tracks()
-        for item in results['items']:
-            track = item['track']
-            print(f"{track['name']} - {track['artists'][0]['name']}")
+        return self.__spotify.current_user_saved_tracks()
 
-    def find_track(self, track):
-        results = self.__spotify.search(q=self.__get_query_string(track), type='track')
+    # Upload track to library in Spotify
+    def add_saved_tracks(self, tracks=None):
+        if not tracks:
+            tracks = self.mapped_tracks
+
+        # We have a uri, just use spotipy to add to saved tracks
+        index = 0
+        while index < len(tracks):
+            chunk_size = min(index + 50, len(tracks))
+            chunk = tracks[index:chunk_size]
+            index = chunk_size
+
+            self.__spotify.current_user_saved_tracks_add([track['uri'] for track in chunk])
+
+    # Given a list of tracks, find there spotify equivalents
+    def find_tracks(self, tracks):
+        for track in tracks:
+            self.__find_track(track)
+
+    # Use spotify to find a track on the spotify service, goal is to retrieve a track uri
+    def __find_track(self, track):
+        results = self.__spotify.search(q=self.__get_query_string(track, blacklist=['year'], strip_brackets=True),
+                                        type='track', limit=10)
 
         if len(results['tracks']['items']) == 0:  # Try again with more lax search terms
-            results = self.__spotify.search(q=self.__get_query_string(track, blacklist=['album', 'year'],
-                                                                     strip_brackets=True), type='track')
+            results = self.__spotify.search(q=self.__get_query_string(track, blacklist=['album', 'artist', 'year'],
+                                                                     strip_brackets=True), type='track', limit=50)
 
         if len(results['tracks']['items']) > 0:
             track_result_list = [self.__map_result_to_track(result) for result in results['tracks']['items']]
             scored_results = {self.__get_match_score(track, result_track):result_track for result_track in track_result_list}
             print(f"Best Matched Result for {track['title']} - {track['artist']} from Spotify is {scored_results[max(scored_results)]}")
+            top_score = max(scored_results)
+            if top_score > 75:
+                self.mapped_tracks.append(scored_results[top_score])
+            else:
+                print("No matches found with a high enough score")
+                self.unmapped_tracks.append(track)
+
         else:
             print(f"Could not find a match for {track['title']} - {track['artist']}")
+            self.unmapped_tracks.append(track)
 
+    # Load spotify config info
     def __load_secrets(self):
         with open('/home/yasir/Documents/Projects/gpm_to_spotify/secrets.json') as secrets_file:
             secrets = load(secrets_file)
@@ -51,6 +84,7 @@ class SpotifyWrapper:
             self.__client_secret = secrets['client']['secret']
             self.__user = secrets['user']
 
+    # Create a formatted query string to find a track on Spotify
     def __get_query_string(self, track, blacklist=[], strip_brackets=False):
         query_parts = []
         for key, value in track.items():
@@ -64,6 +98,7 @@ class SpotifyWrapper:
 
         return ' '.join(query_parts)
 
+    # Map Resulting Spotify track item to a local Track object
     def __map_result_to_track(self, result_track):
         return Track(title=result_track['name'],
                      artist=result_track['artists'][0]['name'],  # TODO: Verify primary artist is always first
