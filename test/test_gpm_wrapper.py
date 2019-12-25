36ff74240cec605ab3c8c90839e7a95f89295bb7
Yasir Mohamed <mohamey@tcd.ie>
2019-12-25 14:21:43 +0000

    Update GPM Test Case, use global config

--- test/test_gpm_wrapper.py
+++ test/test_gpm_wrapper.py
@@ -1,6 +1,7 @@
 import unittest
 from exceptions import gpm_exceptions
 from gmusicapi import Mobileclient
+from meta.structures.track import Track
 from unittest.mock import MagicMock
 from unittest.mock import patch
 from wrappers import gpm_wrapper
@@ -16,7 +17,7 @@ class GpmWrapperTestCase(unittest.TestCase):
     def test_auth_flow_when_oauth_login_complete(self):
         mock_mobile_client = Mobileclient()
         mock_mobile_client.oauth_login = MagicMock(return_value=False)
-        with patch.object(self.wrapper, '_Wrapper__get_oauth_token', return_value=True) as patched_oauth_method,\
+        with patch.object(self.wrapper, 'SpotifyWrapper__', return_value=True) as patched_oauth_method,\
             patch.object(self.wrapper, '_Wrapper__login', return_value=True) as patched_login_method:
 
             auth_result = self.wrapper.handle_auth_flow(self.test_fp, self.device_id)
@@ -51,11 +52,33 @@ class GpmWrapperTestCase(unittest.TestCase):
 
     """Map library to Tracks Test"""
     def test_library_mapped_to_songs(self):
-        self.wrapper.handle_auth_flow("/home/yasir/Documents/Projects/gpm_to_spotify/.runtime/oauth.cred")
-        self.wrapper.get_song_library()
+        songs_list = []
+        # Append a song with all the attributes
+        songs_list.append({
+            'title': 'The Great Destroyer',
+            'artist': 'Nine Inch Nails',
+            'album': 'Year Zero',
+            'year': '2007'
+        })
+
+        songs_list.append({
+            'title': 'Lifeless Dead',
+            'artist': 'Mad Season',
+            'album': 'Above (Deluxe Edition}',
+            'year': None
+        })
+
+        self.wrapper.set_library(songs_list)
         self.wrapper.map_song_library_to_tracks()
 
-        self.assertTrue(len(self.wrapper.tracks) > 0)
+        tracks_list = self.wrapper.get_tracks()
+        self.assertEqual(len(tracks_list), 2)
+
+        for track in tracks_list:
+            self.assertEqual(Track, type(track))
+
+            for key in ['title', 'artist', 'album', 'year']:
+                self.assertTrue(key in track)
 
 
 if __name__ == "__main__":
--- wrappers/gpm_wrapper.py
+++ wrappers/gpm_wrapper.py
@@ -6,28 +6,30 @@ from os import path
 
 
 class Wrapper:
-    def __init__(self):
+    def __init__(self, config):
+        self.__library = []
+        self.__oauth_path = config['oauth_path'] # TODO: This should not go to prod, cred shouldn't be stored on disk
+
         self.mobile_client = Mobileclient()
-        self.library = []
         self.tracks = []
 
-    def handle_auth_flow(self, oauth_file_path, device_id=Mobileclient.FROM_MAC_ADDRESS):
-        if not self.__get_oauth_token(oauth_file_path):
+    def handle_auth_flow(self, device_id=Mobileclient.FROM_MAC_ADDRESS):
+        if not self.__get_oauth_token(self.__oauth_path):
             raise UnableToGetOauthCredentials("Unable to get oauth credentials for GPM")
 
-        self.__login(oauth_file_path, device_id)
+        self.__login(self.__oauth_path, device_id)
 
         # Got both the oauth token, and logged in. Return true to indicate success
         return True
 
     def get_song_library(self):
         if self.mobile_client.is_authenticated():
-            self.library = self.mobile_client.get_all_songs()
+            self.__library = self.mobile_client.get_all_songs()
         else:
             raise NotAuthenticatedException("Unable to perform authenticated requests at this time.")
 
     def map_song_library_to_tracks(self):
-        for track in self.library:
+        for track in self.__library:
             try:
                 self.tracks.append(Track(title=track['title'], artist=track['artist'],
                                          album=maybe(track)['album'],  # By default, if key doesn't exist None is returned
@@ -37,6 +39,9 @@ class Wrapper:
 
     def get_tracks(self):
         return self.tracks
+
+    def set_library(self, library):
+        self.__library = library
 
     def __get_oauth_token(self, oauth_file_path):
         # If token already exists on system, read it and check its not empty
