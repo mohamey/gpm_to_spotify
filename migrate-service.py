d8d6afe6464b5b7a0291d5fa2bf424c64808b8f1
Yasir Mohamed <mohamey@tcd.ie>
2020-01-04 22:55:59 +0000

    Initial working e2e where auth code is received and library written back to kafka

--- migrate-service.py
+++ migrate-service.py
@@ -1,5 +1,5 @@
-from json import load
-from kafka import KafkaConsumer
+from json import load, dumps
+from kafka import KafkaConsumer, KafkaProducer
 from os import path
 from wrappers.gpm_wrapper import Wrapper
 
@@ -18,19 +18,30 @@ if __name__ == '__main__':
         value_deserializer=lambda x: x.decode()
     )
 
+    kafka_producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                                   value_serializer=lambda v: dumps(v).encode('utf-8'))
+
     with open('.config.json') as config_file:
         config = load(config_file)
 
     for message in kafka_consumer:
+        wrapper = Wrapper(config['gpm'])
+        code = str(message.value)
         try:
-            # TODO: It's reading from the kafka topic okay, but we're getting an exception due to redirect uri mismatch
-            wrapper = Wrapper(config['gpm'])
-            wrapper.handle_auth_flow(code=str(message.value))
+            # TODO: We're reading from the Kafka topic okay, but now we need to write the results back to another topic
+            # TODO: We should also multithread this shit.
+            # TODO: Patch gmusicapi to stop it from writing credentials to disk
+            wrapper.handle_auth_flow(code=code)
             wrapper.get_song_library()
             wrapper.map_song_library_to_tracks()
             print(len(wrapper.get_tracks()))
-            print("Finished :)")
+
+            # Throw it back on a topic
         except Exception as e:
             print(f"There was an issue getting the library - {e}")
+        finally:
+            kafka_producer.send(topic="gpm_libraries", value=str(wrapper.get_tracks()), key=code.encode('utf-8'))
+            print("Finished :)")
+
 
         print(message.value)
