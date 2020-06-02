765bf8acb9edcd3dac9ac6374fa2b71410cdccce
Yasir Mohamed <mohamey@tcd.ie>
2020-06-02 22:08:34 +0100

    Create spotify Search Wrapper as required by TDD Cases

--- wrappers/spotify/search_wrapper.py
+++ wrappers/spotify/search_wrapper.py
@@ -1,7 +1,9 @@
-from exceptions.spotify.search_exceptions import NoMatchException
+from exceptions.spotify.search_exceptions import NoMatchException, SpotifyMalformedTrackException
+from fuzzywuzzy import fuzz
 from meta.structures.track import GpmTrack, SpotifyTrack
 from spotipy import Spotify
 from typing import Tuple
+
 
 class SearchWrapper:
     """
@@ -27,23 +29,36 @@ class SearchWrapper:
         """
 
         # Convert GPM Track To Search String
-        search_string: str = SearchWrapper.__get_search_query(gpm_track=GpmTrack)
+        search_string: str = SearchWrapper.__get_search_query(gpm_track=gpm_track)
 
         # Search Spotify API using all available criteria
+        search_results: dict = (spotify_client.search(q=search_string, type='track', limit=50))['tracks']
 
-        # Search Spotify using relaxed criteria if no results returned
+        # If we didn't get any results, relax the critetia and search again
+        if search_results['total'] == 0:
+            search_string: str = SearchWrapper.__get_search_query(gpm_track=gpm_track, attributes_filter=('album',))
+            search_results: dict = spotify_client.search(q=search_string, type='track', limit=50)['tracks']
 
-        # Raise NoMatchException if we still don't have any results
+            if search_results['total'] == 0:
+                # Give up, we still dont have any matches
+                raise NoMatchException(f"No match for {gpm_track.get_title()} - {gpm_track.get_artist()} - \
+                                            {gpm_track.get_album()}")
 
-        # Score our results
+        else:
+            # We have results, parse the items
+            search_results = search_results['items']
 
-        # Pick the result with the highest score
+        # Set the best result to a placeholder with an impossible score
+        best_result: SpotifyTrack = SpotifyTrack(title='Placeholder', artist='Placeholder', uri='test', score=-1)
+        for result in search_results:
+            current_result: SpotifyTrack = SearchWrapper.__parse_result_to_track(result)
+            result_score: int = SearchWrapper.__score_match(gpm_track=gpm_track, spotify_track=current_result)
 
-        # Convert it to a Spotify Track
+            current_result.set_score(score=result_score)
 
-        # Return the track
+            best_result = max(best_result, current_result, key=lambda x: x.get_score())
 
-        return None
+        return best_result
 
     @staticmethod
     def __get_search_query(gpm_track: GpmTrack, attributes_filter: Tuple[str] = ()) -> str:
@@ -81,3 +96,59 @@ class SearchWrapper:
                 query_parts.append(f"album:\"{album_value}\"")
 
         return '+'.join(query_parts)
+
+    @staticmethod
+    def __parse_result_to_track(result: dict) -> SpotifyTrack:
+        """
+        This method should parse a single track result from the Spotify API into a SpotifyTrack object
+
+        Args:
+            result (dict): A Dict representing a track search result from the Spotify API
+
+        Returns:
+            SpotifyTrack: A new Spotify Track representing the search result from Spotify
+
+        Raises:
+            SpotifyMalformedTrackException: Raised by the SpotifyTrack when the result we're trying to parse into the Spotify
+                Track object doesn't meet the constructor's requirements
+
+        """
+
+        return SpotifyTrack(
+            title=result['name'],
+            artist=result['artists'][0]['name'],
+            album=result['album']['name'],
+            album_art_url=result['album']['images'][2]['url'],
+            uri=result['uri'],
+            year=result['album']['release_date'][0:4]
+        )
+
+    @staticmethod
+    def __score_match(gpm_track: GpmTrack, spotify_track: SpotifyTrack) -> int:
+        """
+        Using the gpm_track as the reference object, get each of the gpm track's attributes and compare it with the
+        spotify track using fuzzy matching, then get the average score for each attribute.
+
+        Args:
+            gpm_track (GpmTrack): gpm track we want to compare
+            spotify_track (SpotifyTrack): Spotify Track we want to compare
+
+        Returns:
+            int: Score for the match
+
+        """
+
+        score, common_attributes = 0, 0
+
+        gpm_dict: dict = vars(gpm_track)
+        spotify_dict: dict = vars(spotify_track)
+
+        for key in gpm_dict:
+            gpm_value: str = str(gpm_dict.get(key)).lower()
+            spotify_value: str = str(spotify_dict.get(key)).lower()
+
+            if gpm_value is not None and spotify_value is not None:
+                common_attributes += 1
+                score += fuzz.partial_ratio(gpm_value, spotify_value)
+
+        return int(score / common_attributes)
